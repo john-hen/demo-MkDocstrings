6481ccc6cef08fb4b02c0388049ad76b91128f07
John Hennig <jhen@gmx.net>
2022-02-20 16:56:39 +0100

    Minor clean-up in build script.

--- build.py
+++ build.py
@@ -4,4 +4,4 @@ from subprocess import run
 from pathlib import Path
 
 here = Path(__file__).parent
-process = run(['mkdocs', 'build', '--site-dir', 'HTML'], cwd=here)
+run(['mkdocs', 'build', '--site-dir', 'HTML'], cwd=here)
